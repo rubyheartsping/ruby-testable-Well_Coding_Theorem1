cdf22a3d5f90a70255ceef811c7388815ecf65b2
rubyheartsping <carnelian12@naver.com>
2026-03-23 16:47:34 +0900

    [level 2] Title: 숫자 변환하기, Time: 671.65 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,17 @@
+def solution(x, y, n):
+    INF = float('inf')
+    dp = [INF] * (y + 1)
+    dp[x] = 0
+
+    for i in range(x, y + 1):
+        if dp[i] == INF:
+            continue
+
+        if i + n <= y:
+            dp[i + n] = min(dp[i + n], dp[i] + 1)
+        if i * 2 <= y:
+            dp[i * 2] = min(dp[i * 2], dp[i] + 1)
+        if i * 3 <= y:
+            dp[i * 3] = min(dp[i * 3], dp[i] + 1)
+
+    return dp[y] if dp[y] != INF else -1
