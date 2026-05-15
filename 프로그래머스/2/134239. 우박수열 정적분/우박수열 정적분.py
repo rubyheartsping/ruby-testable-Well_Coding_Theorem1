dea1428bec8380f19767b3656fb5122b47910686
rubyheartsping <carnelian12@naver.com>
2026-05-15 12:41:40 +0900

    [level 2] Title: 우박수열 정적분, Time: 1.05 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
@@ -0,0 +1,32 @@
+def solution(k, ranges):
+    
+    traces = []
+    while k != 1:
+        traces.append(k)
+        
+        if k % 2 == 1:
+            k = 3 * k + 1
+        
+        elif k % 2 == 0:
+            k //= 2
+    
+    traces.append(1)
+    
+    areas = []
+    for i in range(1, len(traces)):
+        areas.append((traces[i] + traces[i-1]) / 2)
+    
+    prefix = [0]
+    for i in range(len(areas)):
+        prefix.append(prefix[-1] + areas[i])
+    
+    result = []
+    for a, b in ranges:
+        actual_b = len(traces) - 1 + b
+        if a > actual_b:
+            result.append(-1)
+        else:
+            result.append(prefix[actual_b] - prefix[a])
+        
+    return result
+        
